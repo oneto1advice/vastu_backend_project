da20b366a27c89c7cdf64e20beb32b46d175a891
oneto1advice <oneto1advice@gmail.com>
2024-07-18 10:58:45 +0530

    little change

--- src/users-service/index.ts
+++ src/users-service/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
